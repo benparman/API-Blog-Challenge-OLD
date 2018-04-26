00d0548128bcdb6869e59a388a8590d017a804a5
Ben Parman <benparman@gmail.com>
2018-04-25 23:47:58 -0600

    Added PUT endpoint

--- server.js
+++ server.js
@@ -14,13 +14,10 @@ app.use(morgan('common'));
 BlogPosts.create('First Post','This is my first blog post!','Ben','2018-04-25');
 
 //--------------------------------------------------------------------------------
-
 app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
 });
-
 //--------------------------------------------------------------------------------
-
 app.post('/blog-posts', jsonParser, (req, res) => {
   const requiredFields = ['title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -34,7 +31,32 @@ app.post('/blog-posts', jsonParser, (req, res) => {
   const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
   res.status(201).json(post);
 });
-
+//--------------------------------------------------------------------------------
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (\`${req.params.id}\`) and request body id (\`${req.body.id}\`) must match!`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
 //--------------------------------------------------------------------------------
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
