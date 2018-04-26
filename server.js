75a4dfb0f01431b11aead43bf94b478f305b6c9c
Ben Parman <benparman@gmail.com>
2018-04-25 22:44:21 -0600

    Added POST endpoint

--- server.js
+++ server.js
@@ -13,10 +13,29 @@ app.use(morgan('common'));
 
 BlogPosts.create('First Post','This is my first blog post!','Ben','2018-04-25');
 
+//--------------------------------------------------------------------------------
+
 app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
 });
 
+//--------------------------------------------------------------------------------
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(post);
+});
+
+//--------------------------------------------------------------------------------
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
